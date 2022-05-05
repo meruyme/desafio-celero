1fc271615aa306d212ee1a38485291cf4e2bc58d
Mellany Linhares <mellinhares6@gmail.com>
2022-05-05 17:42:42 -0300

    feat: Ajuste no settings para permitir paginação

--- desafiocelero/settings.py
+++ desafiocelero/settings.py
@@ -58,7 +58,9 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 MIDDLEWARE = [
